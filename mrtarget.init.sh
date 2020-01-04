fdd4c58fc71fd6e952a33d083d88bdf116f69ca7
eric-czech <eric.allen.czech@gmail.com>
2020-01-04 06:45:15 -0500

    Adding configs and minor changes needed to make pipeline run

---
+++ mrtarget.init.sh
@@ -0,0 +1,8 @@
+export CMD="docker-compose run mrtarget --data-config=mrtarget.data.19.10.yml"
+# $CMD --rea
+# $CMD --hpa
+# $CMD --eco
+# $CMD --efo
+# $CMD --gen
+# Note: custom config used to ignore EuropePMC evidence strings
+$CMD --val
